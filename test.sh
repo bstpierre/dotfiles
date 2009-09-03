29e123b20982202ae7ea00e04f02a133d0fbef56
Brian St. Pierre <brian@bstpierre.org>
2009-09-02 21:04:41 -0400

    All main commands working (init, install, add, push, pull)

--- test.sh
+++ test.sh
@@ -27,10 +27,27 @@ echo testing2 > $HOME/.emacs
 dotfiles add $HOME/.exrc
 dotfiles add $HOME/.emacs
 
+# Save to repo.
+dotfiles push 'New files'
+
 # Pull down "on a new machine".
 export HOME=$home2
 dotfiles install $repo
 diff $home1/.exrc $home2/.exrc
 diff $home1/.emacs $home2/.emacs
+
+# Make changes on the "new machine" and push.
+echo testingAA >> $home2/.exrc
+echo testingBB >> $home2/.emacs
+echo testingCC >> $home2/.foo
+dotfiles add $HOME/.foo
+dotfiles push 'Changed files, added a file'
+
+# Resync on the "original machine".
+export HOME=$home1
+dotfiles pull
+diff $home1/.exrc $home2/.exrc
+diff $home1/.emacs $home2/.emacs
+diff $home1/.foo $home2/.foo
 
 exit 0
